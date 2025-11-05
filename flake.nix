720b6a616eeaf133ba3c22430db874b42d465578
AMikio <shimonishimikio@gmail.com>
2025-11-04 23:04:25 -0300

    style: rename flake templates

--- flake.nix
+++ flake.nix
@@ -1,27 +1,29 @@
 {
-    description = "Development environments for Python and ML.";
+  description = "Development environments for Python and ML.";
 
-    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    outputs = {nixpkgs, ...}: let
-        forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in {
-        templates = {
-            default = {
-                description = ''
-                    Default flake - Basic uv setup with Python.
-                '';
-                path = ./templates/default;
-            };
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in {
+    templates = rec {
+      default = python;
 
-            cuda = {
-                description = ''
-                    CUDA flake - Development with GPU accelerated tools using CUDA.
-                '';
-                path = ./templates/cuda;
-            };
-        };
+      python = {
+        description = ''
+          Default flake - Basic uv setup with Python.
+        '';
+        path = ./templates/python;
+      };
 
-        formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+      python-cuda = {
+        description = ''
+          CUDA flake - Development with GPU accelerated tools using CUDA.
+        '';
+        path = ./templates/python-cuda;
+      };
     };
+
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+  };
 }
